052892bce2743ff889dc2bff8642d5561cad1de8
Katerina Kampardi <kkampardi@gmail.com>
2015-11-27 12:56:44 +0200

    Completed ex 9-4

--- chapter9/ex9-3.py
+++ chapter9/ex9-3.py
@@ -1,3 +1,6 @@
+import string
+
+# handle file open
 filename = "romeo.txt"
 try:
     fp = open( filename, "r")
@@ -5,11 +8,13 @@ except Exception as e:
     print  "File cannot be opened: ", filename
     exit()
 
+# declare structures
 words = list()
 counts = dict()
 
 for line in fp:
-    line = line.rstrip()
+    line = line.translate(None, string.punctuation)
+    line = line.lower()
     words = line.split()
     for word in words:
         if word not in counts:
