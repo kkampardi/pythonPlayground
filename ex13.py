a762864568b0a97e35084f803071ce63d791d261
Katerina Kampardi <kkampardi@gmail.com>
2015-11-27 11:18:16 +0200

    Remove deleted files

--- ex13.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-
-script, first, second, third = argv
-
-print "The script is called ", script
-print "Your first var is ", first
-print "Your second var is ", second
-print "Your third var is ", third
