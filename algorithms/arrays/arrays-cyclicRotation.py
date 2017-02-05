5019e163e25c38cde22c22632c1ee123e25e8402
kkampardi <kkampardi@gmail.com>
2017-02-05 15:51:54 +0200

    Add codilty lessons

---
+++ algorithms/arrays/arrays-cyclicRotation.py
@@ -0,0 +1,7 @@
+def solution(A, K):
+    # write your code in Python 2.7
+    if len(A) == 0:
+        return A
+
+    K = K % len(A)
+    return A[-K:] + A[:-K]
---
+++ algorithms/iterations/iterations-binaryGap.py
@@ -0,0 +1,21 @@
+def solution(N):
+    max_gap = 0
+    current_gap = 0
+
+    # Skip the tailing zero(s)
+    while N > 0 and N%2 == 0:
+        N //= 2
+
+    while N > 0:
+        remainder = N%2
+        if remainder == 0:
+            # Inside a gap
+            current_gap += 1
+        else:
+            # Gap ends
+            if current_gap != 0:
+                max_gap = max(current_gap, max_gap)
+                current_gap = 0
+        N //= 2
+
+    return max_gap
