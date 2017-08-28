d90e3e7bd890bde587e77ffd73c330d69b0f448d
pycat <kkampardi@gmail.com>
2017-08-28 12:13:45 +0300

    Sending emails using Python #1
    - Setup Python to send email using gmail

---
+++ scripts/simple/custom_email.py
@@ -0,0 +1,23 @@
+from smtplib import SMTP, SMTPAuthenticationError, SMTPException
+
+
+host = "smtp.gmail.com"
+port = 587
+username = "hellopycat@gmail.com"
+password =  "katerina123"
+from_email = username
+to_list = ["kkampardi@gmail.com"]
+# setup  the smtp connection
+conn = SMTP(host, port)
+conn.ehlo()
+conn.starttls()
+
+# handle wrong credentials exceptions
+try:
+    conn.login(username, password)
+    conn.sendmail(from_email, to_list, "Hello World")
+except SMTPAuthenticationError:
+    print ("Could not login")
+except:
+    print("an error occured")
+conn.quit()
