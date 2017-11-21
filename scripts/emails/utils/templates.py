ec0868e6d1e747f0d7a15df0c0030d6df41f991c
pycat <kkampardi@gmail.com>
2017-11-21 12:34:28 +0200

    Wrap emails as a python command line programm

--- scripts/emails/utils/templates.py
+++ scripts/emails/utils/templates.py
@@ -1,9 +1,8 @@
-
 import os
 
 
 def get_template_path(path):
-    file_path = os.path.join(os.getcwd(), path)
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), path)
     if not os.path.isfile(file_path):
         raise Exception("This is not a valid template path %s"%(file_path))
     return file_path
@@ -17,15 +16,4 @@ def get_template(path):
 def render_context(template_string, context):
     return template_string.format(**context)
 
-file_ = 'templates/email_message.txt'
-file_html = 'templates/email_message.html'
-template = get_template(file_)
-template_html = get_template(file_html)
-context = {
-    "name": "Justin",
-    "date": None,
-    "total": None
-}
 
-print(render_context(template, context))
-print(render_context(template_html, context))
