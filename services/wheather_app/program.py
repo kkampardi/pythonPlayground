00dfbf3f2062652bb1fd0c44a8a6202967f8684d
pycat <kkampardi@gmail.com>
2018-05-23 15:27:56 +0300

    Finalize whather app

--- services/wheather_app/program.py
+++ services/wheather_app/program.py
@@ -1,6 +1,9 @@
 import bs4
 import requests
+import collections
 
+
+WeatherReport = collections.namedtuple('WeatherReport', 'cond, temp, scale, loc')
 
 def main():
     # print the header
@@ -12,7 +15,13 @@ def main():
     html_data = get_data_from_web(code)
 
     # parse the html
-    get_data_from_html(html_data)
+    report = get_data_from_html(html_data)
+    print('The temp in {} is {} {} and {}'.format(
+        report.loc,
+        report.temp,
+        report.scale,
+        report.cond
+    ))
 
     # display for the forecast
 
@@ -34,10 +43,36 @@ def get_data_from_web(zipcode):
 
 
 def get_data_from_html(html):
-    html = bs4.BeautifulSoup(html, 'html.parser')
-    location = html.find(id='region-content-header').find('h1')
-    print("DEBUG:")
-    print(location)
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    location = soup.find(class_='region-content-header').find('h1').get_text()
+    condition = soup.find(class_='condition-icon').get_text()
+    temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
+    scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
+
+    #location = cleanup_text(location)
+    location = find_city_and_state_from_location(location)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    report = WeatherReport(cond=condition, temp=temp, scale=scale, loc=location)
+    return report
+
+
+def cleanup_text(text):
+    """ cleans up text from white spaces"""
+
+    if not text:
+        return text
+
+    text = text.strip()
+
+    return text
+
+
+def find_city_and_state_from_location(loc: str):
+    parts = loc.split('\n')
+    return parts[0].strip()
 
 
 if __name__ == '__main__':
