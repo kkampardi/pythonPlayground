fe0a9e52f49006634393d392a6ef720409adf4ec
pycat <kkampardi@gmail.com>
2018-08-29 17:12:32 +0300

    adds cashing with scripts

---
+++ scripts/cashing/cashing.py
@@ -0,0 +1,12 @@
+"""
+
+given a URL, try finding that page in the cache
+
+if the page is in the cache:
+    return the cached page
+else:
+    generate the page
+    save the generated page in the cache (for next time)
+    return the generated page
+
+"""
