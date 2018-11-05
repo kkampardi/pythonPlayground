26224cbdcd7e1078b8dd90e8cc637c086f56bf4c
pycat <kkampardi@gmail.com>
2018-11-05 10:21:25 +0200

    wizard game

--- pygames/wizard_game/program.py
+++ pygames/wizard_game/program.py
@@ -1,53 +1,84 @@
+import time
 import random
 
-import time
-
-from pygames.wizard_game.actors import Creature, Wizard
+from actors import Wizard, Creature, SmallAnimal, Dragon
 
 
 def main():
-    header()
-    game_loop()
+	print_header()
+	game_loop()
 
 
-def header():
-    print('-------------------------')
-    print('     Wizard Game App')
-    print('-------------------------')
+def print_header():
+	# Yes, I added this after I recorded the video
+	# but I thought you'd get a kick out if it. ;)
+	print()
+	print('-----------------------------------------------------------------------')
+	print('''
+   (  )   /\   _                 (
+	\ |  (  \ ( \.(               )                      _____
+  \  \ \  `  `   ) \             (  ___                 / _   \\
+ (_`    \+   . x  ( .\            \/   \____-----------/ (o)   \_
+- .-               \+  ;          (  O                           \____
+	 WIZARD BATTLE        )        \_____________  `              \  /
+(__       APP      +- .( -'.- <. - _  VVVVVVV VV V\                 \/
+(_____            ._._: <_ - <- _  (--  _AAAAAAA__A_/                  |
+  .    /./.+-  . .- /  +--  - .     \______________//_              \_______
+  (__ ' /x  / x _/ (                                  \___'          \     /
+ , x / ( '  . / .  /                                      |           \   /
+	/  /  _/ /    +                                      /              \/
+   '  (__/                                             /                  \\
+	''')
+	print()
+	print('-----------------------------------------------------------------------')
+	print()
 
 
 def game_loop():
-    creatures = [
-        Creature('Toad', 1),
-        Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000)
-    ]
+	creatures = [
+		SmallAnimal('Toad', 1),
+		Creature('Tiger', 12),
+		SmallAnimal('Bat', 3),
+		Dragon('Dragon', 50, 75, True),
+		Wizard('Evil Wizard', 1000)
+	]
 
-    print(creatures)
+	# print(creatures)
 
-    hero = Wizard('Gandolf', 75)
-    while True:
-        active_creature = random.choice(creatures)
-        print('A {} of level {} has appeared from a dark and foggy forest ...'
-              .format(active_creature.name, active_creature.level))
+	hero = Wizard('Gandolf', 75)
 
-        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around?')
+	while True:
 
-        if cmd == 'a':
-            if hero.attack(active_creature):
-                creatures.remove(active_creature)
-            else:
-                print("The wizarnd runs and takes time to recover ... ")
-                time.sleep(5)
-                print("The wizard returns revitalized")
-        if cmd == 'r':
-            print('run')
-        elif cmd == 'l':
-            print('The Wizard {} takes in the surroundings and sees:'.format(hero.name))
-            for c in creatures:
-                print(' * A {} of leve {}'.format(c.name, c.level))
+		active_creature = random.choice(creatures)
+		print('A {} of level {} has appear from a dark and foggy forest...'
+			  .format(active_creature.name, active_creature.level))
+		print()
+
+		cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+		if cmd == 'a':
+			if hero.attack(active_creature):
+				creatures.remove(active_creature)
+			else:
+				print("The wizard runs and hides taking time to recover...")
+				time.sleep(5)
+				print("The wizard returns revitalized!")
+		elif cmd == 'r':
+			print('The wizard has become unsure of his power and flees!!!')
+		elif cmd == 'l':
+			print('The wizard {} takes in the surroundings and sees:'
+				  .format(hero.name))
+			for c in creatures:
+				print(' * A {} of level {}'.format(c.name, c.level))
+		else:
+			print("OK, exiting game... bye!")
+			break
+
+		if not creatures:
+			print("You've defeated all the creatures, well done!")
+			break
+
+		print()
+
 
 if __name__ == '__main__':
-    main()
+	main()
